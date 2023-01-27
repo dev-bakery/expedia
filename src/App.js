54edd5c25ee73b562bcba2ac1e6c67b3b864ef56
dev-bakery <rimming2255@naver.com>
2023-01-27 23:52:02 +0900

    -

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import './App.css';
-import Header from './components/header/header';
-import Nav from './components/nav/nav';
+import Header from "./components/header/header";
+import Nav from "./components/nav/nav";
 
 function App() {
   return (
